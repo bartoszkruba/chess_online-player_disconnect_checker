8fb78701429685d41af181d34d9400ee5970677c
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-05 15:09:39 +0200

    Add applicatin properties for dev and prod and implemented database connection

--- src/test/kotlin/com/company/chess_online/disconnect_checker/PlayerDisconnectCheckerApplicationTests.kt
+++ src/test/kotlin/com/company/chess_online/disconnect_checker/PlayerDisconnectCheckerApplicationTests.kt
@@ -3,10 +3,12 @@ package com.company.chess_online.disconnect_checker
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@ActiveProfiles("dev")
 class PlayerDisconnectCheckerApplicationTests {
 
     @Test
