6445891833fb830d675e262d665552ff78d15cb0
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-05 15:19:57 +0200

    Added .travis.yml file to project. #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("mysql:mysql-connector-java")
